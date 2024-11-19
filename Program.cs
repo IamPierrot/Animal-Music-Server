f5f8f6462a8296ec814d54719a36726d6d2f3471
IamPierrot <phatnguyentan836@gmail.com>
2024-11-19 22:40:22 +0700

    upd: add service for Controllers

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddSignalR();
+builder.Services.AddControllers();
 
 var app = builder.Build();
 
